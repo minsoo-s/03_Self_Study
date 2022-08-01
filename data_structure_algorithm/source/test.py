c59871523c5b32276ba703a84c1b53b54be1f872
MINSU-S <alstn6754@outlook.com>
2022-08-01 23:27:47 +0900

    0801_linear_Search

---
+++ data_structure_algorithm/source/test.py
@@ -0,0 +1,15 @@
+Q=input('계산할 숫자:')
+
+# count=0
+# While int(Q)==A:
+if int(Q) < 10:
+    print( 0+int(Q))
+    
+else:
+    n1 = Q[0]
+    n2 = Q[1]
+    A = int(n1)+int(n2)
+    print(A)
+    
+
+    
